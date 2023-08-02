07938c036df5b8725eb07bbc4782049a9554436e
Peter <schmidt.multimedia@gmail.com>
2023-08-02 15:12:34 +0200

    2. Iteration / Polymorphie

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -17,24 +17,22 @@ public class App {
         output("Diff. r-t: " + areaDiff(rectangle, triangle));
         output("Diff. r-c: " + areaDiff(rectangle, circle));
         output("Diff. c-r: " + areaDiff(circle,rectangle));
+        output("Diff. c-t: " + areaDiff(circle, triangle));
+        output("Diff. t-t: " + areaDiff(triangle, triangle));
+
     }
 
     // 1.Iteration:  ggf. 1000x überladen ... 
+    // private static double areaDiff(Circle c, Rectangle r) {
+    //     return c.area() - r.area();
+    // }
 
-    private static double areaDiff(Circle c, Rectangle r) {
-        return c.area() - r.area();
-    }
-
-    private static double areaDiff(Rectangle r, Triangle t) {
-        return r.area() - t.area();
-    }
-    
-    private static double areaDiff(Rectangle r, Circle c) {
-        return r.area() - c.area();
+    // 2. Iteration: Polymorphie - besser!
+    private static double areaDiff(Shape s1, Shape s2) {
+        return s1.area() - s2.area();
     }
 
 
-  
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
